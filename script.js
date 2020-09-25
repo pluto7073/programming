d5c9a4e7c2b3ec5a895bf9b1224db3c6d2c3b413
pluto7073 <62442460+pluto7073@users.noreply.github.com>
2020-09-24 21:43:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function home() {
+	document.location = "index.html";
+}
+function java() {
+	document.location = "java.html";
+}
+function cpp() {
+	document.location = "cpp.html";
+}
+function htmlcss() {
+	document.location = "htmlcss.html";
+}
+function js() {
+	document.location = "js.html";
+}
